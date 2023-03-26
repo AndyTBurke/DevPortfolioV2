75e5419ee8fe59067a5347057d96743126c3ee00
AndyTBurke <107972213+AndyTBurke@users.noreply.github.com>
2023-03-25 19:55:11 -0500

    built-out contact me section

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,36 +1,28 @@
 import React from "react"
+import './styles/Contact.css'
 import email_icon from "./assets/email_icon.png"
 import linkedin_logo from "./assets/linkedin_logo.png"
 import mastodon_logo from "./assets/mastodon_logo.png"
 
 export default function Contact() {
 
-    const containerStyle = {
-        background: "#d3e9f2",
-        width: "45rem",
-        padding: ".7em 1em",
-        borderRadius: "20px",
-        color: "#213547",
-        display: "flex",
-        justifyContent: "space-around",
-        alignItems: "center",
-    }
-
-    const imgStyle = {
-        height: "75px"
-    }
-
     return (
-        <div style={containerStyle}>
-            <a href="mailto: andrewtburke@gmail.com">
-                <img src={email_icon} style={imgStyle} />
-            </a>
-            <a href="https://www.linkedin.com/in/andrew-burke-mke/">
-                <img src={linkedin_logo} style={imgStyle} />
-            </a>
-            <a href="https://techhub.social/@andyb">
-                <img src={mastodon_logo} style={imgStyle} />
-            </a>
+        <div className="contact-container">
+            <h2>Contact Me</h2>
+            <div className="contact-icon-container">
+                <a href="mailto: andrewtburke@gmail.com">
+                    <img src={email_icon} className="contact-images"/>
+                    email
+                </a>
+                <a href="https://www.linkedin.com/in/andrew-burke-mke/">
+                    <img src={linkedin_logo} className="contact-images" />
+                    LinkedIn
+                </a>
+                <a href="https://techhub.social/@andyb">
+                    <img src={mastodon_logo} className="contact-images" />
+                    Mastodon
+                </a>
+            </div>
         </div>
     )
 }
