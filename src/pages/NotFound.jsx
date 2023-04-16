a104a2dc7f3078d0d7f736d7adbdbbc34546b1cd
AndyTBurke <107972213+AndyTBurke@users.noreply.github.com>
2023-04-16 18:48:15 -0500

    feat: back button on not found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,13 +1,15 @@
 import React from "react"
+import {NavLink} from "react-router-dom"
 import oops_renie from "../assets/oops_renie.png"
 import "../styles/NotFound.css"
 
 export default function NotFound() {
     return (
         <div className="not-found-page">
-            <h1>404</h1>
+            <h1>This page doesn't exist!</h1>
             <img src={oops_renie} />
-            <h3>Uh oh! This page doesn't exist!</h3>
+            <h3>Oh no!</h3>
+            <NavLink to="/" className="not-found-return-home">GO HOME!</NavLink>
         </div>
         
     )
